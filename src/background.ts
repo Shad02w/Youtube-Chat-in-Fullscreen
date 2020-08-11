f57b28f4d87f44296ab11665618f4032a3017e13
Shad02w@Win10 <alvistse123@gmail.com>
2020-08-11 18:45:53 +0800

    send whole details object to content script

--- src/background.ts
+++ src/background.ts
@@ -12,23 +12,22 @@ const vidoeRequestFilter = {
 }
 
 const liveChatRequestFilter = {
-    // urls: ['https://www.youtube.com/live_chat/get_live_chat*']
-    // urls: ['https://www.youtube.com/youtubei/v1/live_chat/get_live_chat*']
     urls: ['https://www.youtube.com/*/get_live_chat*']
 
 }
 
 export interface CatchedLiveChatRequestMessage {
-    tabId: number
-    url: string
+    details: chrome.webRequest.WebRequestDetails
 }
+
 
 chrome.webRequest.onBeforeRequest.addListener((details) => {
     if (details.frameId === 0) return
     console.log('/get_live_chat details', details)
+
+
     const message: CatchedLiveChatRequestMessage = {
-        tabId: details.tabId,
-        url: details.url
+        details
     }
     chrome.tabs.sendMessage(details.tabId, message)
 }, liveChatRequestFilter, ['requestBody'])
@@ -41,19 +40,3 @@ chrome.webRequest.onCompleted.addListener((details) => {
 
 }, vidoeRequestFilter)
 
-// Cant catch url request when user refresh the page, which need to inject content script again
-// chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-//     console.log(details)
-//     // send a message to the tab to check whether the content scripted has been injected
-//     chrome.tabs.executeScript(details.tabId, {
-//         file: 'liveChatRequestReplay.js'
-//     })
-// },
-//     {
-//         url: [
-//             {
-//                 urlContains: 'https://www.youtube.com/watch'
-//             }
-//         ]
-//     }
-// )
