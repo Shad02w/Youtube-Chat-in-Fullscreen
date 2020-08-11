4bbc85a7eaad5cdf6651fab4fa377e170e7e18e2
Shad02w@Win10 <alvistse123@gmail.com>
2020-08-11 08:57:37 +0800

    Youtube just changed /get_live_chat api, need to figure something else

--- src/liveChatRequestReplay.ts
+++ src/liveChatRequestReplay.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { CatchedLiveChatRequestMessage } from './background'
 
 /* Replay the get_live_chat xhr request to get the response */
@@ -9,6 +9,7 @@ interface MyWindow extends Window {
 }
 
 declare var window: MyWindow
+
 
 (function () {
 
@@ -21,15 +22,40 @@ declare var window: MyWindow
     // run code here
     console.log('liveChatRequestReplay.js injected')
 
+    // id
+    const addChatListToDOM = async (chatListActions: YoutubeLiveChat.LiveChatContinuationAction[]) => {
+        const videoContainer = document.getElementById('container')
+        console.log(videoContainer)
+        const overlayLiveChatContainer = document.createElement('div')
+        overlayLiveChatContainer.id = 'overlay-live-chat-container'
+        // overlayLiveChatContainer.style.width = '100px'
+        // overlayLiveChatContainer.style.height = '200px'
+        // overlayLiveChatContainer.style.background = 'black'
+        // overlayLiveChatContainer.style.color = '#fff'
+        chatListActions.map(action => {
+            const p = document.createElement('p')
+            p.innerHTML = action.addChatItemAction.item.liveChatTextMessageRenderer.message.runs[0].text
+        })
+
+    }
+
+
     chrome.runtime.onMessage.addListener(async (message: CatchedLiveChatRequestMessage) => {
         console.log(message)
-        const res = await axios.get(message.url)
-        const data = res.data as YoutubeLiveChat.Data
+        // let res: AxiosResponse<YoutubeLiveChat.Data> | undefined = undefined
+        // try {
+        //     res = await axios.post(message.url)
+        // } catch (error) {
+        //     error.response.data
+        // }
+        // if (res === undefined) return
+        // // const data = res.data as YoutubeLiveChat.Data
+        // console.log('data', data)
 
-        if (data.response.continuationContents.liveChatContinuation.actions &&
-            data.response.continuationContents.liveChatContinuation.actions.length === 0) return
-        const { response: { continuationContents: { liveChatContinuation: { actions } } } } = data
-        console.log(actions)
+        // if (data.response.continuationContents.liveChatContinuation.actions &&
+        //     data.response.continuationContents.liveChatContinuation.actions.length === 0) return
+        // const { response: { continuationContents: { liveChatContinuation: { actions } } } } = data
+
     })
 
 
