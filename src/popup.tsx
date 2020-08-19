a4a4f34b89629fed0013a298280a0a932a2d2a3e
mrsheep@Pro13-Alvis <alvistse123@gmail.com>
2020-08-19 09:05:15 +0800

    use material ui compoenents

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,9 +5,16 @@ import './css/popup.css'
 import icon from './images/chat.png'
 import Switch from '@material-ui/core/Switch'
 import Fade from '@material-ui/core/Fade'
+import Typography from '@material-ui/core/Typography'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createMuiTheme()
+theme.typography.h6 = {
+    fontSize: '1.1rem'
+}
 
 const useStyle = makeStyles(theme => {
-    const imageHeight = 1.2
     const normalPadding = 1
     return createStyles({
         container: {
@@ -15,11 +22,12 @@ const useStyle = makeStyles(theme => {
             height: 'auto',
             minHeight: '5rem',
             fontFamily: "'Noto Sans JP', sans-serif",
-            gridGap: '0.6rem',
         },
         icon: {
-            width: imageHeight + 'rem',
-            height: imageHeight + 'rem',
+            // width: imageHeight + 'rem',
+            // height: imageHeight + 'rem',
+            width: theme.spacing(3),
+            height: theme.spacing(3),
             paddingBottom: '0.2rem'
         },
         header: {
@@ -30,9 +38,7 @@ const useStyle = makeStyles(theme => {
             borderBottom: '1px solid #e6e6e6'
         },
         appName: {
-            marginLeft: '0.7rem',
-            fontSize: '1rem',
-            fontWeight: 400
+            marginLeft: theme.spacing(2),
         },
         main: {
             padding: `${normalPadding}rem`,
@@ -40,7 +46,7 @@ const useStyle = makeStyles(theme => {
         part: {
             display: 'flex',
             flexFlow: 'row nowrap',
-            justifyContent: 'space-around',
+            justifyContent: 'space-between',
             alignItems: 'center',
         },
         subHeader: {
@@ -80,21 +86,23 @@ const App: React.FC = () => {
     }, [])
 
     return (
-        <Fade in={isReady} timeout={700}>
-            <div className={classes.container}>
-                <header className={classes.header}>
-                    <img className={classes.icon} src={icon} alt="App icon" />
-                    <h1 className={classes.appName}>Youtube Chat in Fullscreen</h1>
-                </header>
-                <main className={classes.main}>
-                    <article className={classes.part} aria-label="">
-                        <p className={classes.subHeader}>Show chat overlay when fullscreen in every youtube videos page</p>
-                        <Switch checked={isExtEnable} onChange={() => chrome.storage.sync.set({ on: !isExtEnable })} />
-                    </article>
-                </main>
-                <footer></footer>
-            </div>
-        </Fade>
+        <ThemeProvider theme={theme}>
+            <Fade in={isReady} timeout={700}>
+                <div className={classes.container}>
+                    <header className={classes.header}>
+                        <img className={classes.icon} src={icon} alt="App icon" />
+                        <Typography className={classes.appName} variant='h6' color='textPrimary'>Youtube Chat in Fullscreen</Typography>
+                    </header>
+                    <main className={classes.main}>
+                        <article className={classes.part} aria-label="">
+                            <Typography variant='subtitle1' color='textSecondary'>Show chat overlay</Typography>
+                            <Switch checked={isExtEnable} onChange={() => chrome.storage.sync.set({ on: !isExtEnable })} />
+                        </article>
+                    </main>
+                    <footer></footer>
+                </div>
+            </Fade>
+        </ThemeProvider>
     )
 }
 
