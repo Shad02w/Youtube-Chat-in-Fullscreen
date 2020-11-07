2734092e74dc4b55bf7adbef561c2e59fa98894e
Shad02w@Win10 <alvistse123@gmail.com>
2020-11-07 23:27:49 +0800

    using intercept element effect instead of state

--- src/components/hooks/usePlayerState.ts
+++ src/components/hooks/usePlayerState.ts
@@ -1,13 +1,12 @@
-import { useInterceptElementState } from './useElementState'
-import { InterceptedDataElementId_PlayerState, PlayerStateData } from '../../models/Intercept'
-import { YTPlayerState } from '../../models/Player'
-
-
+import { InterceptedDataElementId_PlayerState, PlayerStateData } from '@models/Intercept'
+import { YTPlayerState } from '@models/Player'
+import { useInterceptElementEffect } from './useInterceptElementEffect'
+import { useState } from 'react'
 
 export const usePlayerState = () => {
-    const { data: { state: playerState } } = useInterceptElementState<PlayerStateData>(
-        { state: YTPlayerState.UNSTARTED },
-        InterceptedDataElementId_PlayerState
-    )
+    const [playerState, setPlayerState] = useState<YTPlayerState>(YTPlayerState.UNSTARTED)
+    useInterceptElementEffect<PlayerStateData>(({ state }) => {
+        setPlayerState(state)
+    }, InterceptedDataElementId_PlayerState)
     return { playerState }
 }
