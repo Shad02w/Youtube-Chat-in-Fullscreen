7eed912621784c40aa6585daab17779c5b8f44f8
Shad02w@Pro13 <alvistse123@gmail.com>
2020-11-14 02:24:12 +0800

    restore types file

---
+++ src/types/main.d.ts
@@ -0,0 +1,14 @@
+declare module "*.svg" {
+    const value: any
+    export default value
+}
+
+declare module "*.png" {
+    const value: any
+    export default value
+}
+
+declare module "*.json" {
+    const value: any
+    export default value
+}
