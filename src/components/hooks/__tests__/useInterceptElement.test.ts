a494e7000f9890bb8ea428c606338b8066f80d92
Shad02w@Pro13 <alvistse123@gmail.com>
2020-11-15 22:34:57 +0800

    update the tesing for useInterceptElement hook

--- src/components/hooks/__tests__/useInterceptElement.test.ts
+++ src/components/hooks/__tests__/useInterceptElement.test.ts
@@ -1,28 +1,44 @@
 import { useInterceptElement } from "@hooks/useInterceptElement"
 import { createInterceptElement } from "@models/Intercept"
-import { cleanup, renderHook } from "@testing-library/react-hooks"
+import { cleanup } from "@testing-library/react"
+import '@testing-library/jest-dom';
+import { renderHook, act } from "@testing-library/react-hooks";
 
+// mutation observer can not trigger by innerHTML, innerText and textContent
 describe('useInterceptElement hook testing', () => {
     const id = 'idForElement'
-    const initValue = { message: 'hello' }
+    const helloMessage = { message: 'hello' }
 
     afterEach(() => {
         cleanup()
         document.body.innerHTML = ''
     })
 
-    test('Should return initValue when no intercept element is not exist', () => {
-        const { result } = renderHook(() => useInterceptElement(id, initValue))
-        expect(result.current).toStrictEqual(initValue)
+    test('Should return initValue when no intercept element is not exist', async () => {
+        const { result } = renderHook(() => useInterceptElement(id, helloMessage))
+        expect(result.current).toStrictEqual(helloMessage)
     })
 
-    test('Should return content of intercept element instead of initValue when element already exist', () => {
-        const initValueOfInterceptEl = { message: 'bye' }
-        const interceptEl = createInterceptElement(id, initValueOfInterceptEl)
+    test('Should return content of intercept element instead of initValue when element already exist', async () => {
+        const byeMessage = { message: 'bye' }
+        const interceptEl = createInterceptElement(id, byeMessage)
         interceptEl.mount()
-        const { result } = renderHook(() => useInterceptElement(id, initValue))
-        expect(result.current).toStrictEqual(initValueOfInterceptEl)
 
+        const { result } = renderHook(() => useInterceptElement(id, helloMessage))
+        expect(result.current).toStrictEqual(byeMessage)
     })
+
+    test('Should return new value instead of initValue when content of intercept element update', async () => {
+        const byeMessage = { message: 'bye' }
+        const interceptEl = createInterceptElement(id, byeMessage)
+        interceptEl.mount()
+
+        const { result } = renderHook(() => useInterceptElement(id, helloMessage))
+        await act(async () => {
+            interceptEl.set(helloMessage)
+        })
+        expect(result.current).toStrictEqual(helloMessage)
+    })
+
 
 })
