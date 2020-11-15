ba189fa25103cbca22a71c7a7cc6a17454c71bc5
Shad02w@Pro13 <alvistse123@gmail.com>
2020-11-15 22:33:29 +0800

    get initValue if the intercept element is already exist, and dont check mutation type anymore

--- src/components/hooks/useInterceptElement.ts
+++ src/components/hooks/useInterceptElement.ts
@@ -1,36 +1,16 @@
+import { getInterceptElementContent } from "@models/Intercept"
 import { useEffect, useState } from "react"
 import { useElementStatus } from "./useElementState"
 
-// export const useInterceptElement = <T>(effect: (data: T) => any, id: string) => {
-//     const { ready } = useElementState(id)
-
-//     useEffect(() => {
-//         if (ready) {
-//             const interceptedElment = document.getElementById(id) as HTMLDivElement
-//             const observer = new MutationObserver((mutations) => {
-//                 const childMutation = mutations.find(mutation => mutation.type === 'childList')
-//                 if (!childMutation) return
-//                 const data = JSON.parse((childMutation.target as HTMLDivElement).innerHTML) as T
-//                 effect(data)
-//             })
-//             observer.observe(interceptedElment, { childList: true })
-//             return () => observer.disconnect()
-//         }
-//         // eslint-disable-next-line react-hooks/exhaustive-deps
-//     }, [ready, effect])
-// }
-
 export const useInterceptElement = <T>(id: string, initValue: T) => {
     const { ready } = useElementStatus(id)
-    const [data, setData] = useState<T>(initValue)
+    const [data, setData] = useState<T>(document.getElementById(id) ? getInterceptElementContent(document.getElementById(id)!) : initValue)
 
     useEffect(() => {
         if (ready) {
             const interceptedElment = document.getElementById(id) as HTMLDivElement
-            const observer = new MutationObserver((mutations) => {
-                const childMutation = mutations.find(mutation => mutation.type === 'childList')
-                if (!childMutation) return
-                const json = JSON.parse((childMutation.target as HTMLDivElement).innerHTML) as T
+            const observer = new MutationObserver(() => {
+                const json = getInterceptElementContent(interceptedElment) as T
                 setData(json)
             })
             observer.observe(interceptedElment, { childList: true })
