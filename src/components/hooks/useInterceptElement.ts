78a58d2052ad9c4c2ed4c1203515abc7adc3162c
Shad02w@Pro13 <alvistse123@gmail.com>
2020-11-19 20:56:12 +0800

    change the name of parameter of useInterceptElement as default value

--- src/components/hooks/useInterceptElement.ts
+++ src/components/hooks/useInterceptElement.ts
@@ -2,9 +2,9 @@ import { getInterceptElementContent } from "@models/Intercept"
 import { useEffect, useState } from "react"
 import { useElementStatus } from "./useElementState"
 
-export const useInterceptElement = <T>(id: string, initValue: T) => {
+export const useInterceptElement = <T>(id: string, defaultValue: T): T => {
     const { ready } = useElementStatus(id)
-    const [data, setData] = useState<T>(document.getElementById(id) ? getInterceptElementContent(document.getElementById(id)!) : initValue)
+    const [data, setData] = useState<T>(document.getElementById(id) ? getInterceptElementContent(document.getElementById(id)!) : defaultValue)
 
     useEffect(() => {
         if (ready) {
