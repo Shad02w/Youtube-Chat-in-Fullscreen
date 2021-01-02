8e4e7c83949abf77e7327a06314928dca52480ac
Shad02w@Win10 <alvistse123@gmail.com>
2021-01-03 05:41:22 +0800

    Add outline style to MyButton

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -5,8 +5,20 @@ import { ThemeColor } from '../models/Color'
 export const MyButton = withStyles({
     containedPrimary: {
         backgroundColor: `#${ThemeColor[200]}`,
+        borderWidth: '2px',
         '&:hover': {
-            backgroundColor: `#${ThemeColor[100]}`
+            backgroundColor: `#${ThemeColor[100]}`,
+            borderWidth: '2px',
+        }
+    },
+    outlinedPrimary: {
+        color: `#${ThemeColor[200]}`,
+        borderColor: `#${ThemeColor[200]}`,
+        borderWidth: '2px',
+        '&:hover': {
+            color: `#${ThemeColor[100]}`,
+            borderColor: `#${ThemeColor[100]}`,
+            borderWidth: '2px',
         }
     }
 })(Button)
