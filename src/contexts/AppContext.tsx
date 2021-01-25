82c6f35408f2f364ced434b3e6e1a0536b8a3d3c
Shad02w@Win10 <alvistse123@gmail.com>
2021-01-25 19:11:37 +0800

    Only determine enable chat filter on local, use chat filter settings in chrome stroage

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -4,28 +4,17 @@
 import { useChatBox } from "@hooks/useChatBox";
 import { useFullscreenState } from "@hooks/useFullscreenState";
 import { useUrl } from "@hooks/useUrl";
-import { ChatFilter } from "@models/ChatFilter";
-import React, { createContext, useContext, useMemo, useState } from "react";
+import React, { createContext, useCallback, useContext, useMemo, useState } from "react";
 import { StorageContext } from "./StorageContext";
 
 interface AppContextType {
     showOverlay: boolean
     setShowOverlay: (value: boolean) => any
-    chatFilter: ChatFilter
-    setChatFilter: React.Dispatch<React.SetStateAction<ChatFilter>>
+    enableChatFilter: boolean
+    toggleEnableChatFilter: () => any
 }
 
 type AppContextProviderProps = React.PropsWithChildren<{}>
-
-const ChatFilter_Default: ChatFilter = {
-    guest: true,
-    member: true,
-    moderator: true,
-    owner: true,
-    sticker: true,
-    superchat: true,
-    membership: true
-}
 
 export const AppContext = createContext<AppContextType>({} as AppContextType)
 
@@ -33,17 +22,18 @@ export const AppContextProvider: React.FC<AppContextProviderProps> = ({ children
 
     const [showOverlayPartly, setShowOverlay] = useState<boolean>(false)
     const { storage: { show: show_storage } } = useContext(StorageContext)
-    const [chatFilter, setChatFilter] = useState<ChatFilter>(ChatFilter_Default)
+    const [enableChatFilter, setEnableChatFilter] = useState(false)
     const { isFullscreen } = useFullscreenState()
     const { expanded } = useChatBox()
     useUrl(() => {
-        setChatFilter(ChatFilter_Default)
+        setEnableChatFilter(false)
     })
+    const toggleEnableChatFilter = useCallback(() => setEnableChatFilter(pre => !pre), [setEnableChatFilter])
 
     const showOverlay = useMemo(() => (show_storage && isFullscreen && expanded === true && showOverlayPartly), [showOverlayPartly, show_storage, isFullscreen, expanded])
 
     return (
-        <AppContext.Provider value={{ showOverlay, setShowOverlay, chatFilter, setChatFilter }}>
+        <AppContext.Provider value={{ showOverlay, setShowOverlay, enableChatFilter, toggleEnableChatFilter }}>
             {children}
         </AppContext.Provider>
     )
