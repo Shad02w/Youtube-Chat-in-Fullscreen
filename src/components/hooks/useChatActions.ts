71b30ca317d168360ebcc88b6dcd59afd3bb8e4a
Shad02w@Win10 <alvistse123@gmail.com>
2021-04-10 00:47:11 +0800

    (Refactor) Correct typo

--- src/components/hooks/useChatActions.ts
+++ src/components/hooks/useChatActions.ts
@@ -1,17 +1,15 @@
-import { useState, useCallback } from 'react'
-import {
-    AdvancedChatLiveActions,
-} from '@models/Chat'
-import { ContentScriptWindow } from '@models/Window'
-
-declare const window: ContentScriptWindow
+import { useState, useCallback } from 'react';
+import { AdvancedChatLiveActions } from '@models/Chat';
 
 export const useChatActions = (init: AdvancedChatLiveActions, max: number) => {
-    const [chatActions, setChatActioins] = useState<AdvancedChatLiveActions>(init)
+    const [chatActions, setChatActions] = useState<AdvancedChatLiveActions>(init);
 
-    const update = useCallback((list: AdvancedChatLiveActions) => setChatActioins(pre => pre.concat(list).slice(-max)), [setChatActioins, max])
+    const update = useCallback(
+        (list: AdvancedChatLiveActions) => setChatActions((pre) => pre.concat(list).slice(-max)),
+        [setChatActions, max]
+    );
 
-    const reset = useCallback(() => setChatActioins([]), [setChatActioins])
+    const reset = useCallback(() => setChatActions([]), [setChatActions]);
 
-    return { chatActions, update, reset }
-}
+    return { chatActions, update, reset };
+};
