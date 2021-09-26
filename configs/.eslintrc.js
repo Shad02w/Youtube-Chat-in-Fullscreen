105bfdbd60a8bc171c51c95ba49da61226f30d85
Shad02w@Win10 <alvistse123@gmail.com>
2021-09-26 21:42:50 +0800

    (Refactor) Removed babel and jest config on root workspace and updated eslint

--- configs/.eslintrc.js
+++ configs/.eslintrc.js
@@ -6,10 +6,12 @@ const config = {
         es6: true
     },
     extends: ['plugin:@shad02w/baseline'],
-    plugins: ['import', '@typescript-eslint'],
+    plugins: ['import', '@typescript-eslint', 'react'],
     rules: {
         'import/exports-last': 'off',
-        '@typescript-eslint/no-var-requires': 'off'
+        '@typescript-eslint/no-var-requires': 'off',
+        'react/display-name': 'off',
+        'react/jsx-max-props-per-line': 'off'
     }
 }
 
