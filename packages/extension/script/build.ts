4a1c303eb75fe138fbe199abec0a4101f2c52692
Alvis Tse <alvistse123@gmail.com>
2022-02-02 16:18:58 +0800

    refactor(extension): updated config and build script

---
+++ packages/extension/script/build.ts
@@ -0,0 +1,26 @@
+import { build } from 'esbuild'
+import fs from 'fs/promises'
+import path from 'path/posix'
+
+const outdir = path.resolve(__dirname, '../dist')
+const manifestPath = path.resolve(__dirname, '../src/manifest.json')
+
+const clean = async () => fs.rm(outdir, { recursive: true, force: true })
+const copyManifest = async () => fs.copyFile(manifestPath, path.join(outdir, './manifest.json'))
+
+const buildScript = async () =>
+    build({
+        entryPoints: {
+            background: path.resolve(__dirname, '../src/background.ts'),
+            'content-script': path.resolve(__dirname, '../src/content-script.ts')
+        },
+        outdir
+    })
+
+const run = async () => {
+    await clean()
+    await buildScript()
+    await copyManifest()
+}
+
+run()
